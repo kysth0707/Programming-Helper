6bd32320a7e1ecc101e902f2087a6a3caac64610
Kim TaeHyeong <kysth0707@naver.com>
2022-11-19 21:33:52 +0900

    Translator Update

--- Translator/main.py
+++ Translator/main.py
@@ -1,5 +1,30 @@
 from ModuleTranslator import Translate
+import pyperclip
+# import time
+# from threading import Thread
+from tkinter import Tk
+from tkinter import Text
+from tkinter import END
 
-print(Translate("Hello world!"))
+root = Tk()
+root.geometry("500x100")
+root.wm_attributes("-topmost", 1)
 
-hey:str='string'
+TBox = Text(width=70)
+TBox.pack()
+
+
+LastText = pyperclip.paste()
+while True:
+	try:
+		if LastText != pyperclip.paste():
+			LastText = pyperclip.paste()
+			TranslatedText = Translate(LastText)
+
+			TBox.delete('1.0', END)
+			
+			TBox.insert(END, TranslatedText)
+			root.deiconify()
+	except:
+		pass
+	root.update()
